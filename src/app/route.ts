7b42c34bf39b4d1e42d7ee9a7d18a37d2d904d87
Shivam Dewangan <110464857+shivamdevs@users.noreply.github.com>
2026-02-28 23:43:29 +0530

    Update route.ts

--- src/app/route.ts
+++ src/app/route.ts
@@ -3,6 +3,5 @@ import { ROUTES } from "@/lib/constants";
 
 export async function GET(request: Request) {
 	const url = new URL(request.url);
-	url.pathname = ROUTES.auth;
-	redirect(url.toString());
+	redirect(`${ROUTES.auth}?${url.searchParams.toString()}`);
 }
